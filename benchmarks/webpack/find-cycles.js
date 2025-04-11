06539c9c38ba80ab211cf5ba12334fafec3ab8ea
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-11 21:08:48 +0200

    update benchmark script

--- benchmarks/webpack/find-cycles.js
+++ benchmarks/webpack/find-cycles.js
@@ -1,10 +1,10 @@
 import { readFile } from 'node:fs/promises';
 import path from 'node:path';
 import { performance } from 'node:perf_hooks';
-import { CyclesJohnson, CyclesSimple, DiGraph } from '../../dist/cjs/index.js';
+import { CyclesDFS, CyclesJohnson, DiGraph } from '../../dist/cjs/index.js';
 /** @type {import('../../dist/cjs/index').DiGraph} */
 /** @type {import('../../dist/cjs/index').CyclesJohnson} */
-/** @type {import('../../dist/cjs/index').CyclesSimple} */
+/** @type {import('../../dist/cjs/index').CyclesDFS} */
 
 {
   console.log('----------------------------------------');
@@ -22,12 +22,13 @@ import { CyclesJohnson, CyclesSimple, DiGraph } from '../../dist/cjs/index.js';
   }
 
   const start = performance.now();
-  console.log('Started webpack benchmark with cycle detection = INFINITY');
+  console.log('Started webpack benchmark with cycle detection = INFINITY (Johnson)');
   const cyclesSearch = new CyclesJohnson(graph);
   const cycles = [...cyclesSearch.findCycles()];
   const end = performance.now() - start;
   console.log(`${cycles.length} cycles found`);
   console.log(`Took ${(end / 1000).toFixed(3)} seconds to find cycles on Webpack`);
+
   // find if there are any duplicates
   const duplicates = [];
   const uniqueCycles = [];
@@ -44,6 +45,53 @@ import { CyclesJohnson, CyclesSimple, DiGraph } from '../../dist/cjs/index.js';
   console.log('Duplicates: ', duplicates.length);
   // print all cycles
   console.log('Unique cycles: ', uniqueCycles.length);
+  // Longest cycle
+  const longestCycle = uniqueCycles.reduce((a, b) => (a.length > b.length ? a : b));
+  console.log('Longest cycle: ', longestCycle.length);
+}
+
+{
+  console.log('----------------------------------------');
+  const data = await readFile(path.join(process.cwd(), './webpack.json'));
+  const graph = new DiGraph();
+  // add vertices to the graph
+  for (const [nodeId] of Object.entries(JSON.parse(data))) {
+    graph.addVertices({ id: nodeId });
+  }
+  // add edges to the graph
+  for (const [nodeId, nodeValue] of Object.entries(JSON.parse(data))) {
+    for (const from of nodeValue.adjacentTo) {
+      graph.addEdges({ from: nodeId, to: from });
+    }
+  }
+
+  const start = performance.now();
+  console.log('Started webpack benchmark with cycle detection = INFINITY (DFS)');
+  const cyclesSearch = new CyclesDFS(graph);
+  const cycles = [...cyclesSearch.findCycles()];
+  const end = performance.now() - start;
+  console.log(`${cycles.length} cycles found`);
+  console.log(`Took ${(end / 1000).toFixed(3)} seconds to find cycles on Webpack`);
+
+  // find if there are any duplicates
+  const duplicates = [];
+  const uniqueCycles = [];
+  const uniqueCyclesSet = new Set();
+  for (const cycle of cycles) {
+    const cycleString = [...cycle].sort().join(',');
+    if (uniqueCyclesSet.has(cycleString)) {
+      duplicates.push(cycle);
+    } else {
+      uniqueCyclesSet.add(cycleString);
+      uniqueCycles.push(cycle);
+    }
+  }
+  console.log('Duplicates: ', duplicates.length);
+  // print all cycles
+  console.log('Unique cycles: ', uniqueCycles.length);
+  // Longest cycle
+  const longestCycle = uniqueCycles.reduce((a, b) => (a.length > b.length ? a : b));
+  console.log('Longest cycle: ', longestCycle.length);
 }
 
 {
@@ -63,39 +111,161 @@ import { CyclesJohnson, CyclesSimple, DiGraph } from '../../dist/cjs/index.js';
 
   const start = performance.now();
   console.log('Started webpack benchmark with cycle detection = 500');
-  const cyclesSearch = new CyclesSimple(graph);
+  const cyclesSearch = new CyclesDFS(graph);
   const cycles = [...cyclesSearch.findCycles(500)];
   const end = performance.now() - start;
   console.log(`${cycles.length} cycles found`);
   console.log(`Took ${(end / 1000).toFixed(3)} seconds to find cycles on Webpack`);
+
+  // find if there are any duplicates
+  const duplicates = [];
+  const uniqueCycles = [];
+  const uniqueCyclesSet = new Set();
+  for (const cycle of cycles) {
+    const cycleString = [...cycle].sort().join(',');
+    if (uniqueCyclesSet.has(cycleString)) {
+      duplicates.push(cycle);
+    } else {
+      uniqueCyclesSet.add(cycleString);
+      uniqueCycles.push(cycle);
+    }
+  }
+  console.log('Duplicates: ', duplicates.length);
+  // print all cycles
+  console.log('Unique cycles: ', uniqueCycles.length);
+  // Longest cycle
+  const longestCycle = uniqueCycles.reduce((a, b) => (a.length > b.length ? a : b));
+  console.log('Longest cycle: ', longestCycle.length);
 }
 
 {
   console.log('----------------------------------------');
   const data = await readFile(path.join(process.cwd(), './webpack.json'));
   const graph = new DiGraph();
-  for (const [nodeId, nodeValue] of Object.entries(JSON.parse(data))) {
-    graph.addVertex({ id: nodeId, adjacentTo: nodeValue.adjacentTo });
+  // add vertices to the graph
+  for (const [nodeId] of Object.entries(JSON.parse(data))) {
+    graph.addVertices({ id: nodeId });
   }
+  // add edges to the graph
+  for (const [nodeId, nodeValue] of Object.entries(JSON.parse(data))) {
+    for (const from of nodeValue.adjacentTo) {
+      graph.addEdges({ from: nodeId, to: from });
+    }
+  }
+
   const start = performance.now();
   console.log('Started webpack benchmark with cycle detection = 100');
-  const cycles = graph.findCycles({ maxDepth: 100 });
+  const cyclesSearch = new CyclesDFS(graph);
+  const cycles = [...cyclesSearch.findCycles(100)];
   const end = performance.now() - start;
   console.log(`${cycles.length} cycles found`);
   console.log(`Took ${(end / 1000).toFixed(3)} seconds to find cycles on Webpack`);
+
+  // find if there are any duplicates
+  const duplicates = [];
+  const uniqueCycles = [];
+  const uniqueCyclesSet = new Set();
+  for (const cycle of cycles) {
+    const cycleString = [...cycle].sort().join(',');
+    if (uniqueCyclesSet.has(cycleString)) {
+      duplicates.push(cycle);
+    } else {
+      uniqueCyclesSet.add(cycleString);
+      uniqueCycles.push(cycle);
+    }
+  }
+  console.log('Duplicates: ', duplicates.length);
+  // print all cycles
+  console.log('Unique cycles: ', uniqueCycles.length);
+  // Longest cycle
+  const longestCycle = uniqueCycles.reduce((a, b) => (a.length > b.length ? a : b));
+  console.log('Longest cycle: ', longestCycle.length);
 }
 
 {
   console.log('----------------------------------------');
   const data = await readFile(path.join(process.cwd(), './webpack.json'));
   const graph = new DiGraph();
-  for (const [nodeId, nodeValue] of Object.entries(JSON.parse(data))) {
-    graph.addVertex({ id: nodeId, adjacentTo: nodeValue.adjacentTo });
+  // add vertices to the graph
+  for (const [nodeId] of Object.entries(JSON.parse(data))) {
+    graph.addVertices({ id: nodeId });
   }
+  // add edges to the graph
+  for (const [nodeId, nodeValue] of Object.entries(JSON.parse(data))) {
+    for (const from of nodeValue.adjacentTo) {
+      graph.addEdges({ from: nodeId, to: from });
+    }
+  }
+
   const start = performance.now();
   console.log('Started webpack benchmark with cycle detection = 20');
-  const cycles = graph.findCycles({ maxDepth: 20 });
+  const cyclesSearch = new CyclesDFS(graph);
+  const cycles = [...cyclesSearch.findCycles(20)];
   const end = performance.now() - start;
   console.log(`${cycles.length} cycles found`);
   console.log(`Took ${(end / 1000).toFixed(3)} seconds to find cycles on Webpack`);
+
+  // find if there are any duplicates
+  const duplicates = [];
+  const uniqueCycles = [];
+  const uniqueCyclesSet = new Set();
+  for (const cycle of cycles) {
+    const cycleString = [...cycle].sort().join(',');
+    if (uniqueCyclesSet.has(cycleString)) {
+      duplicates.push(cycle);
+    } else {
+      uniqueCyclesSet.add(cycleString);
+      uniqueCycles.push(cycle);
+    }
+  }
+  console.log('Duplicates: ', duplicates.length);
+  // print all cycles
+  console.log('Unique cycles: ', uniqueCycles.length);
+  // Longest cycle
+  const longestCycle = uniqueCycles.reduce((a, b) => (a.length > b.length ? a : b));
+  console.log('Longest cycle: ', longestCycle.length);
+}
+
+{
+  console.log('----------------------------------------');
+  const data = await readFile(path.join(process.cwd(), './webpack.json'));
+  const graph = new DiGraph();
+  // add vertices to the graph
+  for (const [nodeId] of Object.entries(JSON.parse(data))) {
+    graph.addVertices({ id: nodeId });
+  }
+  // add edges to the graph
+  for (const [nodeId, nodeValue] of Object.entries(JSON.parse(data))) {
+    for (const from of nodeValue.adjacentTo) {
+      graph.addEdges({ from: nodeId, to: from });
+    }
+  }
+
+  const start = performance.now();
+  console.log('Started webpack benchmark with cycle detection = 10');
+  const cyclesSearch = new CyclesDFS(graph);
+  const cycles = [...cyclesSearch.findCycles(10)];
+  const end = performance.now() - start;
+  console.log(`${cycles.length} cycles found`);
+  console.log(`Took ${(end / 1000).toFixed(3)} seconds to find cycles on Webpack`);
+
+  // find if there are any duplicates
+  const duplicates = [];
+  const uniqueCycles = [];
+  const uniqueCyclesSet = new Set();
+  for (const cycle of cycles) {
+    const cycleString = [...cycle].sort().join(',');
+    if (uniqueCyclesSet.has(cycleString)) {
+      duplicates.push(cycle);
+    } else {
+      uniqueCyclesSet.add(cycleString);
+      uniqueCycles.push(cycle);
+    }
+  }
+  console.log('Duplicates: ', duplicates.length);
+  // print all cycles
+  console.log('Unique cycles: ', uniqueCycles.length);
+  // Longest cycle
+  const longestCycle = uniqueCycles.reduce((a, b) => (a.length > b.length ? a : b));
+  console.log('Longest cycle: ', longestCycle.length);
 }
