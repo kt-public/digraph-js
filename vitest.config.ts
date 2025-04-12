24b08007a7dbf6e044704b25e931ff729ab45f63
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-04-12 16:51:36 +0200

    sonar scan

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     coverage: {
       provider: 'istanbul', // or 'v8'
       // provider: 'v8',
+      include: ['src/**/*.{js,ts}'],
       reporter: ['text', 'html', 'clover', 'json', 'lcov']
     }
   }
